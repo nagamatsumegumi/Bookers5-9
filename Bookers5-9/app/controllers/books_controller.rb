de5a610bcc97072be04ac42c8e22a150c1c135ab
Hakata Mentaiko <Mentaiko@NAGAMATSU-no-MacBook-Pro.local>
2019-04-24 17:42:02 +0900

    [finish]5-9

---
+++ Bookers5-9/app/controllers/books_controller.rb
@@ -0,0 +1,51 @@
+class BooksController < ApplicationController
+
+  def index
+    @books = Book.all
+    @book = Book.new
+  end
+
+  def show
+  	@book = Book.find(params[:id])
+    @newbook = Book.new
+  end
+
+  def new
+  	@book = Book.new
+  end
+
+  def create
+        @books = Book.all
+  	    @book = Book.new(book_params)
+        @book.user_id = current_user.id
+     if @book.save
+        redirect_to book_path(@book), notice: "You have creatad book successfully."
+     else
+        render action: :index
+     end
+  end
+
+  def edit
+  	@book = Book.find(params[:id])
+  end
+
+  def update
+  	book = Book.find(params[:id])
+ if book.update(book_params)
+    redirect_to book_path(book), notice: "You have updated book successfully."
+ else
+    render action: :edit
+ end
+  end
+
+  def destroy
+  	book = Book.find(params[:id])
+  	book.destroy
+    redirect_to books_path
+  end
+
+  private
+    def book_params
+      params.require(:book).permit(:title, :opinion)
+  end
+end
---
+++ Bookers5-9/config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '4acb083141a9a4d3d09306456faf7163e83400621edc3c271e0b7444fb113aae0df09ba7935a7d8ed6a51fe44efc441f2b5ed5d1084982a7da62ba5289583830'
